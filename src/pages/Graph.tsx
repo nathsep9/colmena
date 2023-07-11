2386d11622a36512acd76e65eb66f40028637173
Natalia Bello <naliaab1809@gmail.com>
2023-07-10 20:07:54 -0500

    feat: Add deploy configured

--- src/pages/Graph.tsx
+++ src/pages/Graph.tsx
@@ -1,5 +1,3 @@
-import { Container, Box, AppBar } from "@mui/material";
-
 import { Graphics } from "../components/Graphics";
 import graphics from "assets/img/graphics.jpg";
 import { ImageContainer } from "components/ImageContainer";
